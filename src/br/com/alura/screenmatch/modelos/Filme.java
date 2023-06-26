89343ded73dce9fd8c90035b43adf0c8bf27ce7a
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-26 20:48:33 -0300

    adição do atributo no Filme

--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,5 +1,13 @@
 package br.com.alura.screenmatch.modelos;
 
 public class Filme {
+    private String diretor;
 
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
 }
