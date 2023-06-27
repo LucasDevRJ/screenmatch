fd5a417d76bccd44748ca89730bf0dda7e22b75c
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-27 15:21:46 -0300

    método para calcular tempo total de filmes assistidos

--- src/br/com/alura/screenmacth/calculos/CalculadoraDeTempo.java
+++ src/br/com/alura/screenmacth/calculos/CalculadoraDeTempo.java
@@ -1,9 +1,15 @@
 package br.com.alura.screenmacth.calculos;
 
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class CalculadoraDeTempo {
     private int tempoTotal;
 
     public int getTempoTotal() {
         return tempoTotal;
+    }
+
+    public void inclui(Filme filme) {
+        tempoTotal += filme.getDuracaoEmMinutos();
     }
 }
